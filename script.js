62d5b48fe4c5cacbe6b27dba0a53693a05e26c72
reiwg <reiwg1@gmail.com>
2023-01-20 00:24:48 -0300

    Att quarta aula

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ["01-01","02-01","03-01","04-01"],
-    water: ["01-01","02-01","03-01","04-01"],
-    food: ["01-01","02-01","03-01","04-01"],
-    study: ["01-01","02-01","03-01","04-01"],
-    gym: ["01-01","02-01","03-01","04-01"],
+button.addEventListener('click', add) // primeiro é o acontecido, depois ele chama a função do lado
+form.addEventListener("change", save) // se deu o change ele executa a função save
+
+
+function add() {
     
+    const today = new Date().toLocaleDateString("pt-br").slice(0,-5) //Cria uma data, coloca a data em 'pt-br', slice recorta o que vai retornar
+    // const today = "10/01"
+    const dayExists = nlwSetup.dayExists (today)
+    
+    if(dayExists){ //condição, se ela existir executa o que está dentro dessa função
+    alert("Dia já registrado")
+    return // Dentro de uma função essa palavra faz parar o código, ou seja, não passa pra linha de baixo
+    }
 
+    alert("Adicionado com sucesso👌")
+    nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data)) //stringify transforma o dado em texto, ou seja, string
+}
+
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}  //parse transforma o texto em objeto
+ nlwSetup.setData(data)
+ nlwSetup.load() 
